d2824d3f53d7939c6c0800174b0c8e0c8ac4ecb7
DESKTOP-844G8DT\Quentin CAILLET <q.caillet@younup.fr>
2023-02-28 10:25:13 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
+import {ethers} from 'ethers'
+import Token from './artifacts/contracts/MyToken.sol/MyToken.json';
 import './App.css';
 
+const tokenAddress ="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   // ce qui va nous permettre de mettre le nombre de token que possede l'utilisateur.
+   const [balance, setBalance] = useState();
+
+   // a definir 
+     useEffect(() => {
+       getBalance();
+     },[])
+   
+   // function asyncrone qui nous permet de récuperer le nombre de token que possede un utilisateur 
+      async function getBalance() {
+       // verif si l'utilisateur est bien connecter 
+       if(typeof window.etherum !== 'unedefined'){
+         // a definir 
+         const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+         // a definir
+         const provider = new ethers.providers.Web3Provider(window.ethereum);
+         // a definir 
+         const contract = new ethers.Contract(tokenAddress, Token.abi, provider);
+         // a definir 
+         const balance = await contract.balanceOf(accounts[0]);
+         // a definir
+           setBalance(balance / 10 ** 18);
+       }
+     }
+     return (
+       <div className="App">
+         <div> {console.log(balance)}</div>
+         <p> Vous avez {balance} MCO</p>
+       </div>
+     );
 }
 
 export default App;
